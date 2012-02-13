248f5e3c8488ab54198246d95cd88413894eed02
Evgenyi Zaytsev <evgenyi.zaytsev@progforce.com>
2012-02-13 09:53:30 +0400

    added paper-trail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_paper_trail
   
   has_many :jewels
   
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -13,7 +13,7 @@ RailsAdmin.config do |config|
   # config.audit_with :history, Bundle install
   
   # Or with a PaperTrail: (you need to install it first)
-  # config.audit_with :paper_trail, Bundle install
+  config.audit_with :paper_trail
   
   # Set the admin name here (optional second array element will appear in a beautiful RailsAdmin red ©)
   config.main_app_name = ['Delmar', 'Admin']
